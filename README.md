018b3d5f01d85b0de6546ad2aec135a6e278e222
Marc Brüderlin <hello@marcbruederlin.com>
2016-07-12 20:29:45 +0200

    Added gulp watch parameter

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# TODO
+# Comet
+
+A lightweight and robust BEM-style CSS framework.
+
+## Installation
+
+Soon…
+
+## Usage
+
+Soon…
+
+## Documentation
+- Getting Started
+- Typography
+- Buttons
+- Forms
+- Lists
+- Tables
+- Responsive
+- Utilities
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@
 
   var gulp = require('gulp'),
       sass = require('gulp-sass'),
+      argv = require('yargs').argv,
       clean = require('gulp-clean'),
+      watch = require('gulp-watch'),
       rename = require('gulp-rename'),
       cleanCSS = require('gulp-clean-css'),
       autoprefixer = require('gulp-autoprefixer');
@@ -20,12 +22,16 @@
       .pipe(gulp.dest('./dist'));
   });
 
-    gulp.task('build:minify', ['build:sass'], function() {
+  gulp.task('build:minify', ['build:sass'], function() {
     return gulp.src('./dist/*.css')
       .pipe(cleanCSS({compatibility: 'ie10'}))
       .pipe(rename({suffix: '.min'}))
       .pipe(gulp.dest('./dist'));
   });
+
+  if(argv.watch) {
+    gulp.watch('./src/**/*.scss', ['build:sass']);
+  }
 
   gulp.task('build', ['build:clean', 'build:sass', 'build:minify']);
 })();
