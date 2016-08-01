4849e605d7257d70c22eb9fce37be889f7de0cde
Marc Brüderlin <hello@marcbruederlin.com>
2016-08-01 14:43:56 +0200

    Fixed #5

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('.introduction').css('height', window.innerHeight);
-});
-
-$(window).resize(function() {
-  $('.introduction').css('height', window.innerHeight);
-});
